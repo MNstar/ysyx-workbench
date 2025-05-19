200d9048be5fd6c26a548d65311a6059d095b76e
tracer-ysyx <tracer@ysyx.org>
2025-05-19 16:24:22 +0800

    > compile NEMU ysyx_24070009 赵天翼 Linux Tianyi 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:24:22 up 6 days, 13:47,  0 user,  load average: 0.06, 0.17, 0.14

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -27,6 +27,7 @@ image: image-dep
 
 run: insert-arg
 	# echo "TODO: add command here to run simulation"
-	$(MAKE) -C $(NPC_HOME) IMG=$(IMAGE).bin
+	echo $(IMAGE)
+	$(MAKE) -C $(NPC_HOME) sim IMG=$(IMAGE).bin
 
 .PHONY: insert-arg
